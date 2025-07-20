3c34b4f9942c4090bc1c3a8fa02665754c2a5014
Natanael <taelima1997@gmail.com>
2025-07-20 17:41:59 -0300

    initi frontend structure

---
+++ app/routes.ts
@@ -0,0 +1,13 @@
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
+
+export default [
+  layout("layouts/layout.tsx", [
+    index("routes/dashboard.tsx"),
+    route("tasks", "routes/tasks.tsx"),
+  ]),
+] satisfies RouteConfig;
